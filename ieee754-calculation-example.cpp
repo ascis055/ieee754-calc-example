bacc68545a057277ded897b50b6c31ec1d3e6cdf
Alexandra Stankovich <stankovichalexandra@gmail.com>
2024-09-26 23:53:33 -0700

    Calculate floating point value using floating point operations

--- ieee754-calculation-example.cpp
+++ ieee754-calculation-example.cpp
@@ -20,16 +20,27 @@ uint8_t const exp_width = 8U;
 uint8_t const mantissa_width = width - exp_width - 1;
 uint8_t const bias = 127U;
 
-/*
- * *** STUDENTS SHOULD WRITE CODE FOR THIS FUNCTION ***
- * Students should create or add any data structures needed.
- * Students should create or add any functions or classes they may need.
- */
 float ieee_754(uint32_t const data) {
     float value;
-    // This will fail the tests. Students should do the proper IEEE-754 calculation per assignment
-    // using the 32 bit 'data' value passed into this function.
-    value = 1.23;
+
+    uint32_t mantissa;  // 24-bit mantissa: 23 bits from input, 1 bit added
+    uint8_t exp;        // 8 bit unsigned exponent
+    uint8_t sign;       // 1 bit of sign
+
+    /* Extract bit ranges */
+    mantissa = data & ((1 << mantissa_width) - 1);
+    exp = (data >> mantissa_width) & ((1 << exp_width) - 1);
+    sign = data >> (width - 1);
+
+    /* Set implied mantissa bit if exponent is nonzero */
+    if (exp != 0)
+        mantissa |= 1 << mantissa_width;
+
+    /* Calculate value using floating point operations */
+    value = (float) mantissa
+        * powf(2., (float) ((int16_t)exp - bias - mantissa_width))
+        * (sign ? -1. : 1.);
+
     return value;
 }
 
